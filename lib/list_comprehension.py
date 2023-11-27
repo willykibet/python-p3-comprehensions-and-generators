6ad04640fc040611129524e3aaf4200f0eb2ffe3
willykibet <willykipchumba22@gmail.com>
2023-11-27 08:57:55 +0300

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements = [num for num in num_list if num % 2 == 0]
+    
+    return even_elements
 
 def make_exclamation(sentence_list):
-    pass
+    exclamatory_sentences = [sentence + '!' for sentence in sentence_list]
+    
+    return exclamatory_sentences
